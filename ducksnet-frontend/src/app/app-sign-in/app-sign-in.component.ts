46735547d14ae754cb6ab9b316da0c030fb3049c
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-12-22 17:00:51 +0200

    Revert "fix sonarqube"
    This reverts commit 5e34c2a8e7baceae97dedbd4dc076308d54032c7.

--- ducksnet-frontend/src/app/app-sign-in/app-sign-in.component.ts
+++ ducksnet-frontend/src/app/app-sign-in/app-sign-in.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-app-sign-in',
   templateUrl: './app-sign-in.component.html',
+  styleUrls: ['./app-sign-in.component.css']
 })
 export class AppSignInComponent {
 
--- ducksnet-frontend/src/app/apphome/apphome.component.ts
+++ ducksnet-frontend/src/app/apphome/apphome.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-apphome',
   templateUrl: './apphome.component.html',
+  styleUrls: ['./apphome.component.css']
 })
 export class ApphomeComponent {
 
